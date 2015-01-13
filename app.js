6e77284967a7edaec030fa0e86417a98086118cf
Maxwell Da Silva <dayvson@gmail.com>
2015-01-13 15:17:13 -0500

    Adding live and vod commands

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ var helpers = require('./helpers');
 var gear1 = __dirname + "/public/bipbop_4x3/gear1/prog_index.m3u8";
 var startTime = Date.now();
 var playlistInfo = helpers.getSegmentsDuration(gear1);
-var endStream = false;
+var isVOD = false;
 
 app.use(express.static(__dirname + '/public'));
 
+process.on('SIGUSR2', function(){
+  isVOD = false;
+});
+
 process.on('SIGUSR1', function(){
-  endStream = true;
+  isVOD = true;
 });
 
 app.get('/bitrate_:id.m3u8', function(req, res, next){
@@ -26,9 +30,9 @@ app.get('/bitrate_:id.m3u8', function(req, res, next){
   var sequence = totalLoops * playlistInfo.segments.length;
   var currentSegment = 0;
   var segments = [];
-  var m3u8Template,videoPath;
+  var m3u8Template, videoPath, _segment;
   for(var i in playlistInfo.segments){
-    var _segment = playlistInfo.segments[i];
+    _segment = playlistInfo.segments[i];
     deltaInLoop -= _segment;
     if(deltaInLoop > 0){
       currentSegment++;
@@ -40,10 +44,10 @@ app.get('/bitrate_:id.m3u8', function(req, res, next){
     }
   }
   m3u8Template = "#EXTM3U\n#EXT-X-TARGETDURATION:11\n#EXT-X-VERSION:3\n#EXT-X-MEDIA-SEQUENCE:"+(sequence+currentSegment);
-  m3u8Template += "\n"+segments.join('\n');
+  m3u8Template += "\n"+segments.join('\n')+"\n";
 
-  if (endStream) {
-    m3u8Template += "#EXT-X-ENDLIST\n";
+  if (isVOD) {
+    m3u8Template += "#EXT-X-PLAYLIST-TYPE:VOD\n#EXT-X-ENDLIST\n";
   }
 
   res.contentType('application/vnd.apple.mpegurl');
